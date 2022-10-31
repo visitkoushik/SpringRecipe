227221f03a6ef7300a65f63a50d8ebe18b09adfe
visitkoushik <visit.koushik@gmail.com>
2022-10-31 11:58:27 +0530

    Initial Import

---
+++ src/main/java/koushik/recipies/recipies/RecipiesApplication.java
@@ -0,0 +1,13 @@
+package koushik.recipies.recipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koushik/recipies/recipies/RecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package koushik.recipies.recipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
